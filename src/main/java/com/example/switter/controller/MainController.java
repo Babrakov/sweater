8124e12bc4bc280bb7c4fa4105beb4d7a901c2aa
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-04-27 16:47:03 +0300

    Try many to one in hibernate.

--- src/main/java/com/example/switter/controller/MainController.java
+++ src/main/java/com/example/switter/controller/MainController.java
@@ -1,8 +1,10 @@
-package com.example.switter;
+package com.example.switter.controller;
 
 import com.example.switter.domain.Message;
+import com.example.switter.domain.User;
 import com.example.switter.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,29 +14,30 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-public class GreetingController {
+public class MainController {
 
     @Autowired
     private MessageRepository messageRepository;
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Map<String,Object> model) {
-        model.put("name", name);
+    @GetMapping("/")
+    public String greeting() {
         return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("main")
     public String main(Map<String,Object> model){
         Iterable<Message> messages = messageRepository.findAll();
         model.put("messages", messages);
         return "main";
     }
 
-    @PostMapping
-    public String add(@RequestParam String text,
-                      @RequestParam String tag,
-                      Map<String,Object> model){
-        Message message = new Message(text, tag);
+    @PostMapping("main")
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag,
+            Map<String,Object> model){
+        Message message = new Message(text, tag, user);
         messageRepository.save(message);
 
         Iterable<Message> messages = messageRepository.findAll();
